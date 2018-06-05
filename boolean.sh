896b3030241f65a622afeeb27e289e9cd0dd8c5e
GRG23 <greengamer23@gmail.com>
2018-06-05 15:47:00 -0500

    boolean.sh input.sh math.sh variable.sh

---
+++ boolean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ $1 -gt 100 ]
+then
+echo "Thats a big number"
+fi
+echo "Your number was " $1
---
+++ input.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "What are your 3 favorite fruits (in a single line only spaces)?"
+read fruit1 fruit2 fruit3
+
+echo Your three favorite fruits are $fruit1 $fruit2 and $fruit3
